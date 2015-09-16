7cf5973674e9aa0a5b10fa83241b38de8b7d098a
Martin Angers <martin.n.angers@gmail.com>
2015-09-16 13:11:14 -0400

    rehttp: test retry HTTP methods

--- rehttp_test.go
+++ rehttp_test.go
@@ -1,10 +1,12 @@
 package rehttp
 
 import (
+	"net/http"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNoDelay(t *testing.T) {
@@ -51,4 +53,37 @@ func TestExponentialDelay(t *testing.T) {
 		got := fn(nil, nil, i, nil)
 		assert.Equal(t, want[i], got, "%d", i)
 	}
+}
+
+func TestRetryHTTPMethods(t *testing.T) {
+	cases := []struct {
+		retries int
+		meths   []string
+		inMeth  string
+		att     int
+		want    bool
+	}{
+		{retries: 1, meths: nil, inMeth: "GET", att: 0, want: false},
+		{retries: 0, meths: nil, inMeth: "GET", att: 1, want: false},
+		{retries: 1, meths: []string{"get"}, inMeth: "GET", att: 0, want: true},
+		{retries: 1, meths: []string{"GET"}, inMeth: "GET", att: 0, want: true},
+		{retries: 1, meths: []string{"GET"}, inMeth: "POST", att: 0, want: false},
+		{retries: 2, meths: []string{"GET", "POST"}, inMeth: "POST", att: 0, want: true},
+		{retries: 2, meths: []string{"GET", "POST"}, inMeth: "POST", att: 1, want: true},
+		{retries: 2, meths: []string{"GET", "POST"}, inMeth: "POST", att: 2, want: false},
+		{retries: 2, meths: []string{"GET", "POST"}, inMeth: "put", att: 0, want: false},
+		{retries: 2, meths: []string{"GET", "POST", "PUT"}, inMeth: "put", att: 0, want: true},
+	}
+
+	for i, tc := range cases {
+		fn := RetryHTTPMethods(tc.retries, tc.meths...)
+		req, err := http.NewRequest(tc.inMeth, "", nil)
+		require.Nil(t, err)
+		got := fn(req, nil, tc.att, nil)
+		assert.Equal(t, tc.want, got, "%d", i)
+	}
+}
+
+func TestRetryStatus500(t *testing.T) {
+
 }
